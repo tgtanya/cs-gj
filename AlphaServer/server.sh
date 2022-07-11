7493e27818faa4822ae462226a18628df005628a
tgtanya <tgtanya.goel@live.com>
2022-07-12 00:24:33 +0530

    Create server.sh

---
+++ AlphaServer/server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true
+do
+    echo $(echo "Metrics for ssh log-in attempt(s)")
+    echo $(echo "\n")
+    echo $(echo "nodeABC has "; if [ -e /root/nodeABC.log ]; then grep -o -i "failed password\|accepted password" /root/nodeABC.log | wc -l; else echo "0"; fi; echo " attempt(s)")
+    echo $(echo "nodeXYZ has "; if [ -e /root/nodeXYZ.log ]; then grep -o -i "failed password\|accepted password" /root/nodeXYZ.log | wc -l; else echo "0"; fi; echo " attempt(s)"))
+    sleep 1
+done
