ab08237fccd63a0c2cb1917f032cfe4ee9214df2
tgtanya <tgtanya.goel@live.com>
2022-07-12 00:18:58 +0530

    Create client.sh

---
+++ AlphaClient/client.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+nodeLog="/root/client.log"
+while sshpass -p "password" rsync -rvz -e 'ssh -o StrictHostKeyChecking=no -p 22' --progress /var/log/auth.log root@server:$nodeLog 2>&1 >/dev/null | grep -q 'error'
+do
+    sleep 1
+    echo No Connection Established, resending the log...
+done
+
+while inotifywait -q -e modify /var/log/auth.log >/dev/null
+do
+    while sshpass -p "password" rsync -rvz -e 'ssh -o StrictHostKeyChecking=no -p 22' --progress /var/log/auth.log root@server:$nodeLog 2>&1 >/dev/null | grep -q 'error'
+    do
+        sleep 1
+        echo No Connection Established, resending the log...
+    done
+done
